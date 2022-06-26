7c406c0a438251ad05f3d3083a6566cc031d6314
Josue Ramirez <josueramirez0110@gmail.com>
2022-06-26 12:09:36 -0500

    A new route was added to the project and the main styles were modified

---
+++ pages/timeline/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+export default function Timeline({ userName }) {
+  return (
+    <>
+      <h1 className={styles.title}>
+        This is the timeline of {userName}
+      </h1>
+      <Link href='/'>Go home</Link>
+    </>
+  )
+}
+
+Timeline.getInitialProps = () => {
+  return fetch('http://localhost:3000/api/hello')
+    .then(res => res.json())
+}
